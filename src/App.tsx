34df523d8c24a02a106abb3a9f9e7f8762ebc282
happy-cutman <happy.cutman@gmail.com>
2022-11-09 11:53:56 +0300

    ADD | config

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Web3Modal, useAccount, useBalance } from '@web3modal/react'
+import { Web3Modal, useAccount, useBalance, useNetwork, useSwitchNetwork } from '@web3modal/react'
+import { chains, providers } from '@web3modal/ethereum';
 import './App.css'
 import ConnectButton from './components/ConnectButton/ConnectButton';
 
@@ -8,6 +9,17 @@ const config = {
   accentColor: 'teal',
   ethereum: {
     appName: 'web3Modal',
+    chains: [
+      chains.mainnet,
+      chains.goerli,
+      chains.binanceSmartChain,
+      chains.binanceSmartChainTestnet
+    ],
+    providers: [
+      providers.walletConnectProvider({
+        projectId: '0fd4e96476f7e05185797b376aa28781'
+      })
+    ],
     autoConnect: true
   }
 }
@@ -16,13 +28,20 @@ const config = {
 function App() {
   const { account } = useAccount()
   const { data } = useBalance({addressOrName: account?.address})
-  console.log(account)
+  const { error, isLoading,  switchNetwork } = useSwitchNetwork()
+
   return (
     <div className='app-wrapper'>
       <div className='app-content'>
         <ConnectButton/>
         <h3>Account: <span className='info'>{account?.address}</span></h3>
         <h3>Balance: <span className='info'>{data ? data?.formatted : '0.00'}</span></h3>
+        <div className='buttons-container'>
+          <button>Ethereum</button>
+          <button>Goerli</button>
+          <button>Binance main</button>
+          <button>Binance test</button>
+        </div>
       </div>
       {/*@ts-ignore*/}
       <Web3Modal config={config} />
