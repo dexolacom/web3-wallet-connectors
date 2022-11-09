0599404fdbba17bab97c60102c26ceaa61846bf6
happy-cutman <happy.cutman@gmail.com>
2022-11-09 19:53:54 +0300

    ADD | disconnect button

--- src/App.tsx
+++ src/App.tsx
@@ -15,16 +15,16 @@ function App() {
       <div className='app-content'>
         <ConnectButton/>
         <div className='info-container'>
-          <h3>Account: <span className='info'>{stringTrim(address, 10)}</span></h3>
+          <h3>Account: <span className='info'>{address ? stringTrim(address, 10) : '----'}</span></h3>
           <h3>Balance: <span className='info'>{data ? Number(data?.formatted).toFixed(2) : '0.00'}</span></h3>
           <h3>Connected to: <span className='info'>{chain?.name ?? 'unknown'}</span></h3>
         </div>
 
         <div className='buttons-container'>
-          <button onClick={() => switchNetwork?.(1)}>Ethereum</button>
-          <button onClick={() => switchNetwork?.(5)}>Goerli</button>
-          <button onClick={() => switchNetwork?.(56)}>Binance main</button>
-          <button onClick={() => switchNetwork?.(97)}>Binance test</button>
+          <button disabled={chain?.id === 1} onClick={() => switchNetwork?.(1)}>Ethereum</button>
+          <button disabled={chain?.id === 5} onClick={() => switchNetwork?.(5)}>Goerli</button>
+          <button disabled={chain?.id === 56} onClick={() => switchNetwork?.(56)}>Binance main</button>
+          <button disabled={chain?.id === 97} onClick={() => switchNetwork?.(97)}>Binance test</button>
         </div>
       </div>
     </div>
--- src/components/ConnectButton/ConnectButton.tsx
+++ src/components/ConnectButton/ConnectButton.tsx
@@ -1,21 +1,54 @@
 import React from 'react';
-import { useConnect } from 'wagmi';
+import {useAccount, useConnect, useDisconnect} from 'wagmi';
 
 
 const ConnectButton = () => {
+  const { connector: activeConnector } = useAccount()
   const { connect, connectors } = useConnect()
+  const { disconnect } = useDisconnect()
+
+  const MetaMaskConnector = connectors[0]
+  const CoinbaseWalletConnector = connectors[1]
+  const WalletConnectConnector = connectors[2]
 
   return (
-    <div style={{display: 'flex', gap: '20px'}}>
-      {connectors.map((connector) => (
-        <button
-          disabled={!connector.ready}
-          key={connector.id}
-          onClick={() => connect({ connector })}
-        >
-          {connector.name}
-        </button>
-      ))}
+    <div style={{display: 'flex', gap: '15px'}}>
+      {/*{connectors.map((connector) => (*/}
+      {/*  <button*/}
+      {/*    disabled={!connector.ready}*/}
+      {/*    key={connector.id}*/}
+      {/*    onClick={() => connect({ connector })}*/}
+      {/*  >*/}
+      {/*    {connector.name}*/}
+      {/*  </button>*/}
+      {/*))}*/}
+
+      <button disabled={activeConnector?.name === 'MetaMask'} onClick={() => connect({connector: MetaMaskConnector})}>
+        {activeConnector?.name === 'MetaMask'
+          ? <span>Connected</span>
+          : 'Metamask'
+        }
+      </button>
+      <button disabled={activeConnector?.name === 'Coinbase Wallet'} onClick={() => connect({connector: CoinbaseWalletConnector})}>
+        {activeConnector?.name === 'Coinbase Wallet'
+          ? <span>Connected</span>
+          : 'Coinbase'
+        }
+      </button>
+      <button disabled={activeConnector?.name === 'WalletConnect'} onClick={() => connect({connector: WalletConnectConnector})}>
+        {activeConnector?.name === 'WalletConnect'
+          ? <span>Connected</span>
+          : 'WalletConnect'
+        }
+      </button>
+
+      <button style={{display: 'flex'}} onClick={() => disconnect()}>
+        <svg height='25px' width='25px' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#6ee7b7"
+             className="w-6 h-6">
+          <path stroke-linecap="round" stroke-linejoin="round"
+                d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"/>
+        </svg>
+      </button>
 
       {/*{error && <div style={{color: 'crimson', position: 'absolute', top: '90px'}}>{error.message}</div>}*/}
     </div>
